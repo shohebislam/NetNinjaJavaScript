6d694d3b1d240a62165a73e0023a95e8e525ec73
Shoheb Islam <shoheb123.i@gmail.com>
2022-03-07 23:06:43 -0500

    commit 1

---
+++ OdinFundamentalsPart2/sandbox.js
@@ -0,0 +1,33 @@
+// let str = "Apple, Banana, Kiwi";
+// let part= str.slice(-12)
+// console.log(part)
+
+// //substring() is the same as slice() but it can's accept negative indexes
+
+// //substr() similar to slice but the second parameter is for the length of the extracted part.
+
+// let text = "Please visit Microsoft.com"
+// let newText = text.replace(/microsoft/i,"W3Schools")
+// console.log(newText)
+// console.log(text)
+
+
+// let hour = 126;
+// if (hour<18){
+//     console.log("It'a a good day")
+
+// }else{
+//     console.log("Sucks to be you because it's going to be a terrible day")
+// }
+
+
+// // || = OR; && = AND;
+// let firstName ='';
+// let lastName= "";
+// let nickName ="SuperCoder";
+
+// alert(firstName || lastName || nickName||"Anonymous");
+
+//   if (!(age>=14 && age<=90)){
+
+//   }
